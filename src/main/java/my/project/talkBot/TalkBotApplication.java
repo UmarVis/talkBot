5907889675e7b10365aa30a7ea9e5f152dc0041b
UmarVis <umar_9292@mail.ru>
2023-06-02 10:38:20 +0300

    TalkBot

---
+++ src/main/java/my/project/talkBot/TalkBotApplication.java
@@ -0,0 +1,20 @@
+package my.project.talkBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+public class TalkBotApplication {
+
+	public static void main(String[] args) {
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new Bot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/my/project/talkBot/TalkBotApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.talkBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
